09782d9eb31080d846e12c011e59dc7e4640e34b
woosa <wusha152@163.com>
2015-07-30 09:45:49 +0800

    ng显示

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ var app = express();
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 
-// ÖÐ¼ä¼þ
+// ï¿½Ð¼ï¿½ï¿½
 app.use(log4js.connectLogger(logger.express));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Â·ÓÉ
+// Â·ï¿½ï¿½
 routes(app);
 
 // catch 404 and forward to error handler
