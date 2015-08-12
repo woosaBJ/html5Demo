cfa16eb28e1e01d001b80e25df195d7443cedd46
woosa <wusha152@163.com>
2015-08-12 18:28:03 +0800

    刮刮卡中奖次数和数量限制

--- conf/config.js
+++ conf/config.js
@@ -2,15 +2,17 @@ var conf = {
     SERVER_PORT: 8083,
 
     MYSQL: {
-        //DB: 'vc',
-        //PASSWORD: 'wusha',
-        //HOST: '10.10.38.211',
-        //USER: 'wusha',
-        USER: 'root',
-        HOST: 'localhost',
         PORT: 3306,
-        PASSWORD: '',
-        DB: 'woosa',
+        DB: 'vc',
+        PASSWORD: 'wusha',
+        HOST: '10.10.38.211',
+        USER: 'wusha',
+
+        //USER: 'root',
+        //HOST: 'localhost',
+        //PASSWORD: '',
+        //DB: 'woosa',
+
         LOGGING: false
     },
 
